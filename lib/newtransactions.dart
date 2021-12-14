70402b5b840dbc7235b65ef393c642d6cd876c49
Icyy <adityaauty9@gmail.com>
2021-12-14 14:44:57 +0530

    added list view and fixed input values

--- lib/newtransactions.dart
+++ lib/newtransactions.dart
@@ -6,6 +6,15 @@ class NewTransactions extends StatelessWidget {
   final Function newtx;
   NewTransactions({required this.newtx, Key? key}) : super(key: key);
 
+  void submitData() {
+    final String txt = textController.text;
+    final double amt = double.parse(amountController.text);
+    if (txt.isEmpty || amt.isNegative) {
+      return;
+    }
+    newtx(txt, amt);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -17,18 +26,20 @@ class NewTransactions extends StatelessWidget {
             child: TextField(
               decoration: InputDecoration(labelText: 'Title'),
               controller: textController,
+              onSubmitted: (_) => submitData(),
+              keyboardType: TextInputType.name,
             ),
           ),
           Card(
             child: TextField(
               decoration: InputDecoration(labelText: 'Amount'),
               controller: amountController,
+              onSubmitted: (_) => submitData(),
+              keyboardType: TextInputType.number,
             ),
           ),
           ElevatedButton(
-            onPressed: () {
-              newtx(textController.text, double.parse(amountController.text));
-            },
+            onPressed: submitData,
             child: Text("Add Transaction"),
             style: ElevatedButton.styleFrom(primary: Colors.orange[900]),
           )
--- lib/transactionslist.dart
+++ lib/transactionslist.dart
@@ -9,45 +9,50 @@ class TransactionsList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: transactions.map((transaction) {
-        return Card(
-          child: Row(
-            children: [
-              Container(
-                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
-                child: Text(
-                  '${transaction.amount} ₹',
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontWeight: FontWeight.bold,
+    return Container(
+      height: 180,
+      child: ListView.builder(
+        itemBuilder: (ctx, index) {
+          return Card(
+            child: Row(
+              children: [
+                Container(
+                  margin: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+                  child: Text(
+                    '${transactions[index].amount.toStringAsFixed(2)} ₹',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
-                ),
-                decoration: BoxDecoration(
-                  border: Border.all(
-                      color: Colors.deepOrange,
-                      width: 2,
-                      style: BorderStyle.solid),
-                ),
-                padding: EdgeInsets.all(6),
-              ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    transaction.name,
-                    style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                        color: Colors.deepOrange,
+                        width: 2,
+                        style: BorderStyle.solid),
                   ),
-                  Text(
-                    DateFormat.yMMMMd().format(transaction.date),
-                    style: TextStyle(color: Colors.grey),
-                  )
-                ],
-              )
-            ],
-          ),
-        );
-      }).toList(),
+                  padding: EdgeInsets.all(6),
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      transactions[index].name,
+                      style:
+                          TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      DateFormat.yMMMMd().format(transactions[index].date),
+                      style: TextStyle(color: Colors.grey),
+                    )
+                  ],
+                )
+              ],
+            ),
+          );
+        },
+        itemCount: transactions.length,
+      ),
     );
   }
 }
